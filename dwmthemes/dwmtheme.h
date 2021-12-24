50fd2ad2a4fd58a29d8fb276a57bcf2912ca584e
victor-solis <vs.victorsolis@gmail.com>
2021-12-24 11:06:10 -0800

    add themefolder

---
+++ dwmthemes/dwmtheme.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]	="#222222";
+static const char col_gray2[]	="#444444";
+static const char col_gray3[]	="#bbbbbb";
+static const char col_gray4[]	="#eeeeee";
+static const char col_cyan5[]	="#005577";
+static const char col_cyan[]	="#005577";
