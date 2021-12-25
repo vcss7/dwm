bf8138ed2b89858b657e0863668d4690d8ae9199
victor-solis <79403133+victor-solis@users.noreply.github.com>
2021-12-24 17:03:37 -0800

    more themes

---
+++ dracula.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#282a36";
+static const char col_gray2[]       = "#644475";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#282a36";
+static const char col_cyan5[]       = "#a272a4";
+static const char col_cyan[]        = "#bf93f9";
