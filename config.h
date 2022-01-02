c477f2098fab872425e2d6448f30ee26b95d265a
victor-solis <vs.victorsolis@gmail.com>
2022-01-02 07:47:47 -0800

    alpha patch applied

--- config.h
+++ config.h
@@ -4,12 +4,14 @@
 #include "dwmthemes/darkblue.h" /* Include one of the files in the themes folder */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const unsigned int borderpx    = 3;        /* border pixel of windows */
+static const unsigned int snap        = 32;       /* snap pixel */
+static const int showbar              = 1;        /* 0 means no bar */
+static const int topbar               = 1;        /* 0 means bottom bar */
+static const char *fonts[]            = { "monospace:size=10" };
+static const char dmenufont[]         = "monospace:size=10";
+static const unsigned int baralpha    = 0xd0;
+static const unsigned int borderalpha = 200;
 /* Original colors */
 // static const char col_gray1[]       = "#222222";
 // static const char col_gray2[]       = "#444444";
@@ -21,6 +23,11 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan5 },
+};
+static const unsigned int alphas[][3] = {
+  /*              fg      bg        border      */
+  [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+  [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
